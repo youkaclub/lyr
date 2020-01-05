d9df6f22b5429ec9c0962c57a39048e2e5eded35
youka <youka.club@gmail.com>
2020-01-06 00:31:24 +0200

    ..

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(readme_filename, encoding='utf-8') as f:
 
 setup(
     name='lyr',
-    version='0.0.1',
+    version='0.0.2',
     description='lyrics providers',
     long_description=long_description,
     long_description_content_type='text/markdown',
